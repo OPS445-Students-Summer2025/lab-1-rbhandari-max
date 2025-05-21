5e36a9e786273058fb8f82cb72aeac6ebf56c301
rbhandari17 <rbhandari17@myseneca.ca>
2025-05-21 14:32:54 -0400

    Added lab1a

---
+++ lab1c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Description: Script to practice integer objects.'''
+
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Description: Practice math operations and output formatted results.'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+print(f"{x} + {y} * {z} = {result}")
